7356d5c0a0fa8aa7ba5fc6b2fd321d0f0f1e969b
user7 <michael345@gmail.com>
2013-03-22 08:35:01 +0200

    ,

---
+++ Sroot.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	char c = getchar();
+	printf("hello world\n");
+	printf("%c\n", c);
+	/*char d = 'b';*/
+	printf("hello again\n");
+	/*printf("%c", d);*/
+	return 0;
+}
